159a92e309b9956c5222f06c85634f7b96a539ed
ariefrahmanfajri <18523092@students.uii.ac.id>
2020-06-22 16:47:23 +0700

    BabyController store function

--- app/Http/Controllers/BabyController.php
+++ app/Http/Controllers/BabyController.php
@@ -9,4 +9,33 @@ class BabyController extends Controller
     public function index(){
         return view('babies.index');
     }
+
+    public function store(Request $request){
+        $huruf = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+        $uniqueCode = '';
+        for($i=0; $i<10; $i++){
+            $angka = rand(0, 25);
+            $uniqueCode += $huruf[$angka];
+        }
+
+        $this->validate($request,[
+            'nama_anak' => 'required',
+            'jenis_kelamin' => 'required',
+            'tanggal_lahir' => 'required',
+            'nama_ibu' => 'required',
+            'kontak' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        Baby::create([
+            'baby_name'=> $request->nama_anak,
+            'mother_name' => $request->nama_ibu,
+            'baby_birthday' => $request->tanggal_lahir,
+            'address' => $request->alamat,
+            'gender' => $request->jenis_kelamin,
+            'contact' => $request->kontak,
+            'unique_code' => $uniqueCode
+        ]);
+    }
 }
